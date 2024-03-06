e37f752761e5c5b23f4bbfa796267a16bdffe033
Bjoe2703 <162432767+Bjoe2703@users.noreply.github.com>
2024-03-06 13:25:44 +0100

    Update Networking.py

--- PythonKode/Networking.py
+++ PythonKode/Networking.py
@@ -1,12 +1,18 @@
+import socket
+
 def findPorts(port="35565"):
     pass
 
 class NetConecter():
     def __init__(self, port = "35565"):
         self.port = port
+        self.socket = socket.socket()
 
     def openPort(self):
-        pass
+        self.socket.bind(('', self.port))
+
+    def connectPort(self, portAddress, port):
+        self.socket.connect((portAddress, port))
 
 
 
