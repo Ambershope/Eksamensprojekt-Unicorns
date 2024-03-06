edd2588fb9de1842f8132a803062d08e87b6cdd8
Bjoe2703 <162432767+Bjoe2703@users.noreply.github.com>
2024-03-06 13:03:38 +0100

    Update Networking.py

--- PythonKode/Networking.py
+++ PythonKode/Networking.py
@@ -1,5 +1,14 @@
 def findPorts(port="35565"):
     pass
 
-def openPort():
-    pass
+class NetConecter():
+    def __init__(self, port = "35565"):
+        self.port = port
+
+    def openPort(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    print("Hello world")
