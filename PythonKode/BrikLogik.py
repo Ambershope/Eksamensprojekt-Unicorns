59fcee8252e5d032d97d8026f9c17192783362fb
DaTofDOT <162432730+DaTofDOT@users.noreply.github.com>
2024-03-12 23:47:30 +0100

    Main loop and pygame setup

--- PythonKode/BrikLogik.py
+++ PythonKode/BrikLogik.py
@@ -6,3 +6,11 @@ class Piece:
         #self.effectFunction = 
         self.explainingText = "lol no text"
 
+class GameState:
+    def __init__(self, field_, playerPile_):
+        self.field=field_
+        self.playerPile=playerPile_
+        self.turnCycleStep=0
+        self.turnCycleTable=["Select First Player", "Draw Start Hands", 
+                             "You Select Piece", "You Select Tile", "Send To Opponent", "Piece ETB (A)", "Test Win(A)", 
+                             "Wait For Opponent", "Piece ETB (B)", "Test Win (B)" ]
--- PythonKode/main.py
+++ PythonKode/main.py
@@ -1,25 +1,106 @@
 import pygame
+import random
 import AiOponent
 import BrikLogik
 import GameObjects
 import Database
 import Networking
-from Constants import FPS
-
-pygame.init()
+from Constants import *
 
 
-screensize = (1200, 600)
-screen = pygame.display.set_mode(screensize)
-clock = pygame.time.Clock()
+
+class Inputs:
+    def __init__(self):
+        self.quit=False
+        self.mousePosition=[-1,-1]
+        self.mouseLeftButtonDown=False
+
+        #only active for a single frame
+        self.mouseLeftButtonClick=False
+
+    def update(self):
+        self.mouseLeftButtonClick=False
+        self.mousePosition=pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit=True
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse=pygame.mouse.get_pressed()
+                if self.mouseLeftButtonDown == False and mouse[0] == True:
+                    self.mouseLeftButtonDown = True
+                    self.mouseLeftButtonClick = True
+                
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouse=pygame.mouse.get_pressed()
+                if mouse[0] == False:
+                    self.mouseLeftButtonDown = False
+
+        
 
 def main():
-    startGame()
+    global frameCounter
+    #main program loop
+    #Updates the input and calls either game() or startscreen() every frame
+    while True:
+        Input.update()
+
+        if Input.quit == True:
+            return "close game"
+        
+        if screenSelector == "game":
+            game()
+        elif screenSelector == "start":
+            startScreen()
+        else:
+            #this should NOT happen
+            print("Error: screenSelector variable =",screenSelector)
+        
+
+        pygame.display.update()
+        clock.tick(FPS)
+        frameCounter += 1
+
+def game():
+    '''
+Core game logic, called every frame while in game
+\nAlso calls drawGame()
+    '''
+    if Input.mouseLeftButtonClick == True:
+        global screenSelector
+        screenSelector ="start"
+    drawGame()
+
+#visuals for the core game
+def drawGame():
+    global frameCounter
+    brightness=abs(255-((frameCounter*3)%511))
+    screen.fill((0,brightness,0))
+
+
+def startScreen():
+    '''
+Stuff for while on the start screen should
+\nhappen within this function, including drawing it
+    '''
+    if Input.mouseLeftButtonClick == True:
+        global screenSelector
+        screenSelector ="game"
+    drawStartScreen()
+
+#visuals for the startscreen    
+def drawStartScreen():
+    global frameCounter
+    brightness=abs(255-((frameCounter*3)%511))
+    screen.fill((0,0,brightness))
+
+
 
 
 def startGame():
     while True:
-        userInput()
+        #userInputs
         drawBrick()
         sendTurn()
         if checkWin(): 
@@ -30,13 +111,8 @@ def startGame():
             opponentWin()
             break
 
-        drawStep()
 
-def userInput():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pass
-
+'''#Whats a brick?'''
 def drawBrick():
     pass
 
@@ -53,15 +129,20 @@ def checkWin():
 def sendTurn():
     pass
 
-def drawStep():
-    pygame.display.update()
-    clock.tick(FPS)
-
 def youWin():
     pass
 
 def opponentWin():
     pass
+
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Unicorn Fight Club")
+clock = pygame.time.Clock()
+frameCounter=1
+screenSelector="start"
+Input=Inputs()
 
 main()
 pygame.quit()
