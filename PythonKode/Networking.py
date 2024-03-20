d3010b6cb7582df1078f3e2d94f96008b0bc8d3e
Bjoe2703 <162432767+Bjoe2703@users.noreply.github.com>
2024-03-20 10:08:20 +0100

    Net stuf

--- PythonKode/Networking.py
+++ PythonKode/Networking.py
@@ -1,11 +1,25 @@
 import socket
 import threading
 
+def connectTest(sock, addr, port):
+    try:
+        sock.connect((addr, port))
+        print("Port " + str(port) + " open on address: " + addr)
+    except:
+        pass
+
+
 def findPorts(port=35565):
     '''
     Finds a list of open ports on current internet
     \n*port is the port to search on. Default 35565.
     '''
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ipconst = socket.gethostbyname(socket.gethostname()).split(".")
+    ipconst = ipconst[0] + "." + ipconst[1]
+    for i in range(256):
+        for j in range(256):
+            threading.Thread(target=connectTest, args=(sock, ipconst + ".{}.{}".format(i, j), port)).start()
     pass
 
 class NetConnecter():
@@ -18,6 +32,9 @@ class NetConnecter():
         self.stopServer = False
         self.client = None
         self.addr = None
+
+    def sendTurn(self, message):
+        pass
 
     def openPort(self):
         '''
@@ -38,6 +55,7 @@ class NetConnecter():
         '''
         try:
             self.socket.connect((portAddress, port))
+            print(portAddress, port)
         except:
             print("Port not open on", portAddress, ":", port)
 
@@ -54,6 +72,7 @@ if __name__ == "__main__":
         print("Server is starting")
         serverNet = NetConnecter()
         threading.Thread(target=serverNet.openPort).start()
+        
         pass
 
     from tkinter import *
@@ -62,13 +81,15 @@ if __name__ == "__main__":
     root.geometry("500x100")
     f1 = Frame(root)
     e1 = Entry(f1, width=50, justify=CENTER)
-    e1.insert(END, '127.0.0.1')
+    e1.insert(END, socket.gethostbyname(socket.gethostname()))
     b1 = Button(f1, text="Start Client", command=lambda: startClient(e1.get()))
     b2 = Button(f1, text="Start 'Server'", command=startServer)
+    b3 = Button(f1, text="Find open ports", command=lambda: threading.Thread(target=findPorts, args=()).start())
     l1 = Label(f1, text=" --- Netværks text --- ")
     f1.pack()
-    l1.grid(row=0, column=0, columnspan=2)
+    l1.grid(row=0, column=0, columnspan=3)
     b1.grid(row=1, column=0)
     b2.grid(row=1, column=1)
-    e1.grid(row=2, column=0, columnspan=2)
+    b3.grid(row=1, column=2)
+    e1.grid(row=2, column=0, columnspan=3)
     root.mainloop()
