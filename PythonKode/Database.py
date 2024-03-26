ee44bf96e02430456cafa226f1f7d584f9d18461
Ambershope <148982579+Ambershope@users.noreply.github.com>
2024-03-26 17:26:43 +0100

    skrevet noter

--- PythonKode/Database.py
+++ PythonKode/Database.py
@@ -1,12 +1,17 @@
 import os
 
+
+# Finder pathen til en fil, som fx. kortene. Det gør vi fordi programmet kan ligge forskellige steder
+def pathToGameDataFile(folder, fileName, fileType = ".txt"):
+    # Finder først pathen til denne fil
+    path = os.path.dirname(__file__).strip("\PythonKode")
+
+    # Derefter finder den pathen til den ting vi vil havde relativt til denne fil
+    relativePath = "\GameData\\" + folder + "\\" + fileName + fileType
+    return path + relativePath
+
+# Nogle gange vil vi gerne kun finde en folder (som fx til databaser)
 def pathToGameFolder(folder):
     path = os.path.dirname(__file__).strip("\PythonKode")
     relativePath = "\GameData\\" + folder
-    return path + relativePath
-
-
-def pathToGameDataFile(folder, fileName, fileType = ".txt"):
-    path = os.path.dirname(__file__).strip("\PythonKode")
-    relativePath = "\GameData\\" + folder + "\\" + fileName + fileType
     return path + relativePath
---
+++ PythonKode/Initialization.py
@@ -0,0 +1,3 @@
+import sqlite3
+
+conn = sqlite3.connect('C:\Users\oscar\Documents\GitHub\Eksamensprojekt-Unicorns\GameData\Databases\PiecesDatabase.db')
