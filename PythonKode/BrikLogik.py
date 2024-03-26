85fadf142a36e63f7a902106b9b640774a184bb2
Ambershope <148982579+Ambershope@users.noreply.github.com>
2024-03-26 21:34:26 +0100

    database stuff

--- PythonKode/BrikLogik.py
+++ PythonKode/BrikLogik.py
@@ -6,13 +6,12 @@ class Piece:
         print(cardData)
         self.pieceName = cardData[1]
         print(self.pieceName)
-        self.pieceName = pieceId_
         self.isYours=isYours_
         self.persuasion= [cardData[2], cardData[3], cardData[4], cardData[5]]
         self.artworkPath = cardData[6]
         # self.artwork = 
         self.effectFunctionId = cardData[7]
-        self.explainingText = cardData[8]
+        self.flavorText = cardData[8]
 
 class GameState:
     def __init__(self, field_, playerPile_):
@@ -23,4 +22,4 @@ class GameState:
         self.turnCycleTable=["You Select Piece", "You Select Tile", "Send To Opponent", "Piece ETB (A)", "Test Win(A)", "Draw Card", 
                              "Wait For Opponent", "Piece ETB (B)", "Test Win (B)"]
 
-print(Piece(1).pieceName)
+print(Piece(1).flavorText)
