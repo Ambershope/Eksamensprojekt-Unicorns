b2539d5ce6f0e34c1292345210e94c2738bb7519
Ambershope <148982579+Ambershope@users.noreply.github.com>
2024-03-26 14:24:15 +0100

    Ændret varchar til int

--- PythonKode/PieceDatabase.sql
+++ PythonKode/PieceDatabase.sql
@@ -4,14 +4,14 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 
 CREATE TABLE pieces (
-    `pieceId` varchar(2),
+    `pieceId` int,
     `pieceName` varchar(20),
-    `north` varchar,
-    `east` varchar,
-    `south` varchar,
-    `west` varchar,
+    `north` int,
+    `east` int,
+    `south` int,
+    `west` int,
     `artPath` varchar(30),
-    `effektId` varchar(2),
+    `effektId` int,
 
 )
 
