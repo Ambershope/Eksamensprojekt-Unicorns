e527f21a592f048fab8817a2e87a09bef20b6044
Bjoe2703 <bjoern.norby.htx@gmail.com>
2024-03-26 12:22:23 +0100

    Update Networking.py

--- PythonKode/Networking.py
+++ PythonKode/Networking.py
@@ -5,7 +5,7 @@ To start Create a NetConnecter objekt, this objekt will hold all networking proc
 
 import socket
 import threading
-import time
+from time import sleep
 
 
 class NetConnecter():
@@ -18,19 +18,29 @@ class NetConnecter():
         self.socketUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.stopServer = False
         self.client = None
-        self.addr = None
+        self.cliAddr = None
+        self.addr = (socket.gethostbyname(socket.gethostname()), self.port)
         self.addrUDP = ('224.0.2.60', 34543)
         self.timeToLive = 2   # Router Jump Points Allowed
         self.broadcastingUDP = True
+        self.listerUDP = True
         self.openServers = []
+
+    # ----------   General functions   ----------
+    
 
 
     # ----------   UDP Port functions   ----------
     def broadcastServer(self):
+        '''
+        This function starts a TCP server and then broadcast the ip, for this computer.\n
+        
+        '''
         print("Server starting to broadcast")
-        self.socketUDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.timeToLive)
+        threading.Thread(target=self.openTCPPort).start()
         self.broadcastingUDP = True
-        threading.Thread(target=self.broadcastServerTreadUDP)
+        self.socketUDP.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.timeToLive)
+        threading.Thread(target=self.broadcastServerTreadUDP).start()
 
     def broadcastServerTreadUDP(self):
         '''
@@ -39,25 +49,55 @@ class NetConnecter():
         It is part of broadcastServer, and will crash the program if called.
         '''
         while self.broadcastingUDP:
-            self.socketUDP.sendto("I am open :)".encode("utf-8"), self.addrUDP)
-            time.sleep(1.5)
+            print(str("O:" + self.addr[0] + ":" + str(self.addr[1])))
+            self.socketUDP.sendto(str("O:" + self.addr[0] + ":" + str(self.addr[1])).encode("utf-8"), self.addrUDP)
+            sleep(1.5)
 
     def serverLister(self):
-        print("Server starting to listen")
-        self.socketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socketUDP.bind(('', self.addrUDP[1]))
-        mreq = socket.inet_aton(self.addrUDP[0]) + socket.inet_aton('0.0.0.0')
-        self.socketUDP.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-        threading.Thread(target=self.serverListerTreadUDP)
+        '''
+        This function lists servers open, servers need to broadcast that they are open before they are listed.\n
+        If this function is called multiple times it prints 'Server already listening'.\n
+        To find / use the list, it is stored in openServers.
+        '''
+        try:
+            self.listerUDP = True
+            self.socketUDP.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            try: self.socketUDP.bind(('', self.addrUDP[1]))
+            except: pass
+            mreq = socket.inet_aton(self.addrUDP[0]) + socket.inet_aton('0.0.0.0')
+            self.socketUDP.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+            print("Server starting to listen")
+            threading.Thread(target=self.serverListerTreadUDP).start()
+        except:
+            print("Server already listening")
     
     def serverListerTreadUDP(self):
-        while True:
-            print(self.socketUDP.recv(1024))
-            pass
+        '''
+        This function should not be called, except as a thread. \n
+        To turn of, run leaveServerLister \n
+        It is not recommended to turn of the thread by setting listerUDP to false \n
+        It is part of serverLister, and will crash the program if called.
+        '''
+        while self.listerUDP:
+            message = self.socketUDP.recv(1024).decode('utf-8')
+            print(message)
+            message = message.split(':')
+            if message[0] == 'O':
+                addr = (message[1], message[2])
+                if addr not in self.openServers: self.openServers.append(addr)
+            print(self.openServers)
     
     def leaveServerLister(self):
-        mreq = socket.inet_aton(self.addrUDP[0]) + socket.inet_aton('0.0.0.0')
-        self.socketUDP.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+        '''
+        This command stops the server from listing servers.\n
+        Nothing happens if the program is not listing servers.
+        '''
+        try:
+            self.openServers = []
+            self.listerUDP = False
+            mreq = socket.inet_aton(self.addrUDP[0]) + socket.inet_aton('0.0.0.0')
+            self.socketUDP.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+        except: pass # This code will run if the lister is ofline.
         print("Server stopped listening")
 
 
@@ -65,7 +105,7 @@ class NetConnecter():
     def sendTurn(self, message):
         pass
 
-    def resiveTurn(self, processFunk):
+    def resiveTurn(self, procesFunk):
         pass
 
     def openTCPPort(self):
@@ -74,49 +114,50 @@ class NetConnecter():
         This port is only talked throug 1 on 1 channel.\n
         This function should not be used with it self.\n
         '''
-        addr = (socket.gethostbyname(socket.gethostname()), self.port)
-        self.socketTCP.bind(addr)
-        print("Server open on", addr[0], ":", addr[1])
-        self.socketTCP.listen()
-        self.client, self.addr = self.socketTCP.accept()
-        print("Got connection from", self.addr)
+        try:
+            self.addr = (socket.gethostbyname(socket.gethostname()), self.port)
+            self.socketTCP.bind(self.addr)
+            print("Server open on", self.addr[0], ":", self.addr[1])
+            self.socketTCP.listen()
+            self.client, self.cliAddr = self.socketTCP.accept()
+            print("Got connection from", self.cliAddr)
+        except: print("Server already open!")
 
-    def connectTCPPort(self, portAddress, port = 35565):
+    def connectTCPPort(self, portAddress):
         '''
         This function should not be used on your own.
         \nInstead use connect().
         '''
         try:
-            self.socketTCP.connect((portAddress, port))
-            print(portAddress, port)
+            self.socketTCP.connect((portAddress, self.port))
+            print(portAddress, self.port)
         except:
-            print("Port not open on", portAddress, ":", port)
+            print("Port not open on", portAddress, ":", self.port)
 
 
 
 
 
 if __name__ == "__main__":
+
+    netCon = NetConnecter()
+
     def startClient(addr):
         print("Client is starting")
-        clientNet = NetConnecter()
-        clientNet.connectPort(addr)
+        netCon.connectTCPPort(addr)
         pass
 
     def startServer():
         print("Server is starting")
-        serverNet = NetConnecter()
-        threading.Thread(target=serverNet.openPort).start()
+        threading.Thread(target=netCon.openTCPPort).start()
         
     def clientMessage():
         pass
 
-    udpTest = NetConnecter()
-
     from tkinter import *
     root = Tk()
     root.title("Networking Test")
-    root.geometry("500x100")
+    root.geometry("500x120")
     cL = None
     f1 = Frame(root)
     e1 = Entry(f1, width=50, justify=CENTER)
@@ -124,9 +165,9 @@ if __name__ == "__main__":
     e2 = Entry(f1, width=50, justify=CENTER)
     b1 = Button(f1, text="Start Client", command=lambda: startClient(e1.get()))
     b2 = Button(f1, text="Start 'Server'", command=startServer)
-    b3 = Button(f1, text="Find open ports", command=lambda: udpTest.serverLister())
-    b4 = Button(f1, text="UDP messaging", command=lambda: udpTest.broadcastServer())
-    b5 = Button(f1, text="Stop finding ports", command=lambda: udpTest.leaveServerLister())
+    b3 = Button(f1, text="Find open ports", command=lambda: netCon.serverLister())
+    b4 = Button(f1, text="UDP messaging", command=lambda: netCon.broadcastServer())
+    b5 = Button(f1, text="Stop finding ports", command=lambda: netCon.leaveServerLister())
     # b3 = Button(f1, text="Find open ports", command=lambda: threading.Thread(target=findPorts, args=()).start())
     l1 = Label(f1, text=" --- Netværks text --- ")
     f1.pack()
