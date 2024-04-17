c57da56bedd23337e5adfdb229b7b1be2494191c
Ambershope <148982579+Ambershope@users.noreply.github.com>
2024-04-17 13:29:20 +0200

    pewpew

--- PythonKode/Initialization.py
+++ PythonKode/Initialization.py
@@ -25,12 +25,12 @@ def innitialise():
 
     cursor.execute('''INSERT OR REPLACE INTO pieces(pieceId, pieceName, north, east, south, west, artPath, effektId, flavorText) VALUES
                 (1, 'Totally unicorn', 3, 1, 0, 1, 'totallyUnicorn', 0, 'You can be anything you want to be!'),
-                (2, 'Fire Breathing Unicorn', 1, 3, 1, 2, 'FireBreathingUnicorn', 0, 'Refreshing in winter'),
+                (2, 'Fire Breathing Unicorn', 1, 3, 1, 2, 'FireBreathingUnicorn', 3, 'Refreshing in winter'),
                 (3, 'Normie', 0, 0, 0, 0, 'Normiecorn', 1, 'Wait, jeg troede dette var lol'), 
                 (4, 'Coolicorn', 0, 1, 3, 1, 'CoolUnicorn', 0, '(⌐■_■)'), 
                 (5, 'Ice breathing Unicorn', 1, 2, 1, 3, 'FrostUnicorn', 0, 'Refreshing in summer'), 
-                (6, 'Gib Duocorn', 4, 1, 4, 1, 'BigCorn', 2, 'One of a kind'), 
-                (7, 'Unicake', 2, 2, 2, 2, 'Unicake', 0, 'Mums!'), 
+                (6, 'Big Corn', 4, 1, 4, 1, 'BigCorn', 0, 'One of a kind'), 
+                (7, 'Unicake', 2, 2, 2, 2, 'Unicake',0, 'Mums!'), 
                 (8, 'Sleepiecorn', 1, 2, 4, 1, 'SleepyUnicorn', 0, 'Nimble like a cat, and sleepy too'), 
                 (9, 'Sea Unicorn', 1, 2, 3, 3, 'SeaUnicorn', 0, 'Very nimble in the sea'), 
                 (10, 'Rainbowsprint', 1, 1, 3, 2, 'RainbowSprint', 2, 'Legally distinct'), 
@@ -50,7 +50,8 @@ def innitialise():
     
     # Effekt 0: No effect
     # Effekt 1: (normie aura): Reduce attack of surrounding Unicorns to 1
-    # Effekt 2:  (legendary )Romves all other [Cardname] from the battlefield
+    # Effekt 2: (legendary )Romves all other [Cardname] from the battlefield
+    # Effekt 3: Attacks 2 tiles from tile
     # Template (id, 'Name', north, east, south, west, artpath, effektId, 'flavorText'),
 
     # Commit changes
