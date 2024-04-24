e3d417c0f6d1ceb688896dcc8a14bce076c74acf
Bjoe2703 <162432767+Bjoe2703@users.noreply.github.com>
2024-04-24 14:14:37 +0200

    FLUTERNEJ

--- PythonKode/main.py
+++ PythonKode/main.py
@@ -366,10 +366,6 @@ Knapperne = KnappeDetection()
 #midlertidig gameState, ændres inden et spil startes
 gameState=BrikLogik.GameState(GameObjects.Field(1),GameObjects.Pile("Default"))
 
-fluttersej = BrikLogik.Piece(gameState.playerPile.drawPiece(),False)
-gameState.holdingPiece = fluttersej
-gameState.placePiece((2,4))
-
 with open(Database.pathToGameDataFile("Databases", "Settings"), "r") as settingsFile:
     while True:
         setting = settingsFile.readline()
