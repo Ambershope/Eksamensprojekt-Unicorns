593716effc522707e433439717782c99a40df611
Bjoe2703 <162432767+Bjoe2703@users.noreply.github.com>
2024-04-30 13:30:13 +0200

    Update Button.py

--- PythonKode/Button.py
+++ PythonKode/Button.py
@@ -3,18 +3,15 @@ import pygame
 
 class ButtonDetection:
     def __init__(self):
-        self.buttonCollisionTest
-        self.knap
-        self.MousePosition = [0,0]
+        self.mousePosition = [0,0]
 
 
-    def buttonCollisionTest(self, cornorButtonPositionA, cornorButtonPositionB, useUserMouse = True):
+    def buttonCollisionTest(self, cornorButtonPositionA: tuple, cornorButtonPositionB: tuple, useUserMouse = True):
         if useUserMouse == True:
-            MousePosition = pygame.mouse.get_pos()
+            mousePosition = pygame.mouse.get_pos()
     
-        if MousePosition[0] >= min(cornorButtonPositionA[0], cornorButtonPositionB[0]) and MousePosition[0] <= max(cornorButtonPositionA[0], cornorButtonPositionB[0]):
-                
-            if MousePosition[1] >= min(cornorButtonPositionA[1], cornorButtonPositionB[1]) and MousePosition[1] <= max(cornorButtonPositionA[1], cornorButtonPositionB[1]):
+        if mousePosition[0] >= min(cornorButtonPositionA[0], cornorButtonPositionB[0]) and mousePosition[0] <= max(cornorButtonPositionA[0], cornorButtonPositionB[0]):
+            if mousePosition[1] >= min(cornorButtonPositionA[1], cornorButtonPositionB[1]) and mousePosition[1] <= max(cornorButtonPositionA[1], cornorButtonPositionB[1]):
                 return True
         return False
 
